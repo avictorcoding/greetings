6de35fde61db349acd6524afd1ee9ad3e1ce6599
avictorcoding <avictorcoding@gmail.com>
2022-12-29 11:38:22 +0530

    version 17

--- src/main/java/com/example/greetingsdemo/controller/GreetingsController.java
+++ src/main/java/com/example/greetingsdemo/controller/GreetingsController.java
@@ -1,7 +1,5 @@
 package com.example.greetingsdemo.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +10,6 @@ public class GreetingsController {
 	//get all employees
 	@GetMapping("/welcome")
 	String getEmployees() {
-
 		return "hello world";
 	}
 
